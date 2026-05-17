845b5a118964bf7e260063809c567efdf24e69b9
vijaym105 <malusarevijay10@gmail.com>
2026-05-17 15:51:56 +0530

    err resolved

--- Frontend insta-clone/src/features/auth/services/api.auth.jsx
+++ Frontend insta-clone/src/features/auth/services/api.auth.jsx
@@ -1,27 +1,23 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/auth"  && import.meta.env.VITE_API_URL + "/api/auth",
+    baseURL: import.meta.env.VITE_API_URL
+        ? import.meta.env.VITE_API_URL + "/api/auth"
+        : "http://localhost:3000/api/auth",
     withCredentials: true
 })
+
 export async function registerUser(username, email, password) {
     const res = await api.post('/register', { username, email, password })
-    return res.data  
+    return res.data
 }
 
 export async function loginUser(email, password) {
     const res = await api.post('/login', { email, password })
-    return res.data  
+    return res.data
 }
 
 export async function getMe() {
     const res = await api.get('/getMe')
-    return res.data
-}
-
-
-export async function getMe(){
-    const res = await api.get('/getMe')
-
     return res.data
 }
