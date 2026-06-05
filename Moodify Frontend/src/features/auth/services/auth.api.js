90195a0768c8061f89b6697c6c28e4809d455de9
vijaym105 <malusarevijay10@gmail.com>
2026-06-05 17:09:56 +0530

    Auth baseURL

--- Moodify Frontend/src/features/auth/services/auth.api.js
+++ Moodify Frontend/src/features/auth/services/auth.api.js
@@ -1,9 +1,11 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
-    withCredentials: true
-})
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  withCredentials: true,
+});
+
+export default api;
 
 export async function register({ email, password, username }) {
     const response = await api.post("/api/auth/register", {
