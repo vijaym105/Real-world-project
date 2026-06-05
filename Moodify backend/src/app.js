8ddeaa35b349c442839f8eddbad58ba305698109
vijaym105 <malusarevijay10@gmail.com>
2026-06-05 16:52:27 +0530

    Added Render URL

--- Moodify backend/src/app.js
+++ Moodify backend/src/app.js
@@ -7,10 +7,15 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://real-world-project-delta.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
 
 /**
