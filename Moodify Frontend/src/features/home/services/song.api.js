f7be8e6fd5debd57becca9f68a68811d9d31ee0f
vijaym105 <malusarevijay10@gmail.com>
2026-06-07 16:17:47 +0530

    song URL fixed

--- Moodify Frontend/src/features/home/services/song.api.js
+++ Moodify Frontend/src/features/home/services/song.api.js
@@ -1,14 +1,13 @@
-import axios from "axios";
+import axios from "axios"
 
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: BASE_URL,
     withCredentials: true
 })
 
-
 export async function getSongs({ mood }) {
     const response = await api.get("/api/song?mood=" + mood)
-    console.log(response)
     return response.data
 }
